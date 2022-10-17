b9e9b03d853bb32713c15845046c92da3024cf19
marvinvillanea <marvinvillanea1@gmail.com>
2022-10-17 08:34:26 +0800

    update mokers pc

---
+++ lib/pages/forgot_password_verification_page.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:spc_almuni/common/theme_helper.dart';
+import 'package:otp_text_field/otp_field.dart';
+import 'package:otp_text_field/style.dart';
+
+import 'profile_page.dart';
+import 'widgets/header_widget.dart';
+
+class ForgotPasswordVerificationPage extends StatefulWidget {
+  const ForgotPasswordVerificationPage({Key? key}) : super(key: key);
+
+  @override
+  _ForgotPasswordVerificationPageState createState() => _ForgotPasswordVerificationPageState();
+}
+
+class _ForgotPasswordVerificationPageState extends State<ForgotPasswordVerificationPage> {
+  final _formKey = GlobalKey<FormState>();
+  bool _pinSuccess = false;
+
+  @override
+  Widget build(BuildContext context) {
+    double _headerHeight = 300;
+
+    return Scaffold(
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(
+                height: _headerHeight,
+                child: HeaderWidget(
+                    _headerHeight, true, Icons.privacy_tip_outlined),
+              ),
+              SafeArea(
+                child: Container(
+                  margin: const EdgeInsets.fromLTRB(25, 10, 25, 10),
+                  padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                  child: Column(
+                    children: [
+                      Container(
+                        alignment: Alignment.topLeft,
+                        margin: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: const [
+                            Text('Verification',
+                              style: TextStyle(
+                                  fontSize: 35,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black54
+                              ),
+                              // textAlign: TextAlign.center,
+                            ),
+                            SizedBox(height: 10,),
+                            Text(
+                              'Please wait for the approval for the Administrator and check the Email. Thank you!',
+                              style: TextStyle(
+                                // fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black54
+                              ),
+                              // textAlign: TextAlign.center,
+                            ),
+                          ],
+                        ),
+                      ),
+                      const SizedBox(height: 40.0),
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        )
+    );
+  }
+}
---
+++ lib/pages/login_page.dart
@@ -0,0 +1,188 @@
+import 'dart:convert';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:spc_almuni/common/theme_helper.dart';
+
+/*import 'forgot_password_page.dart';
+
+import 'registration_page.dart';
+import 'widgets/header_widget.dart';*/
+import 'widgets/header_widget.dart';
+import 'profile_page.dart';
+import 'forgot_password_verification_page.dart';
+import 'registration_page.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final double _headerHeight = 250;
+  final Key _formKey = GlobalKey<FormState>();
+
+  TextEditingController user = TextEditingController();
+  TextEditingController pass = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: _headerHeight,
+              child: HeaderWidget(_headerHeight, true,
+                  Icons.login_rounded), //let's create a common header widget
+            ),
+            SafeArea(
+              child: Container(
+                  padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+                  margin: const EdgeInsets.fromLTRB(
+                      20, 10, 20, 10), // This will be the login form
+                  child: Column(
+                    children: [
+                      const Text(
+                        'Welcome to SPC',
+                        style: TextStyle(
+                            fontSize: 30, fontWeight: FontWeight.bold),
+                      ),
+                      const Text(
+                        'Sign in into your account',
+                        style: TextStyle(color: Colors.grey),
+                      ),
+                      const SizedBox(height: 30.0),
+                      Form(
+                          key: _formKey,
+                          child: Column(
+                            children: [
+                              Container(
+                                child: TextField(
+                                  autofocus: false,
+                                  controller: user,
+                                  decoration: ThemeHelper().textInputDecoration(
+                                      'Email', 'Enter your Email'),
+                                ),
+                                decoration:
+                                    ThemeHelper().inputBoxDecorationShaddow(),
+                              ),
+                              const SizedBox(height: 30.0),
+                              Container(
+                                child: TextField(
+                                  controller: pass,
+                                  obscureText: true,
+                                  decoration: ThemeHelper().textInputDecoration(
+                                      'Password', 'Enter your password'),
+                                ),
+                                decoration:
+                                    ThemeHelper().inputBoxDecorationShaddow(),
+                              ),
+                              const SizedBox(height: 15.0),
+                              Container(
+                                margin:
+                                    const EdgeInsets.fromLTRB(10, 0, 10, 20),
+                                alignment: Alignment.topRight,
+                                child: GestureDetector(
+                                  onTap: () {
+                                    // Navigator.push( context, MaterialPageRoute( builder: (context) => ForgotPasswordPage()), );
+                                  },
+                                  child: const Text(
+                                    "Forgot your password?",
+                                    style: TextStyle(
+                                      color: Colors.grey,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              Container(
+                                decoration:
+                                    ThemeHelper().buttonBoxDecoration(context),
+                                child: ElevatedButton(
+                                  style: ThemeHelper().buttonStyle(),
+                                  child: Padding(
+                                    padding: const EdgeInsets.fromLTRB(
+                                        40, 10, 40, 10),
+                                    child: Text(
+                                      'Sign In'.toUpperCase(),
+                                      style: const TextStyle(
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.white),
+                                    ),
+                                  ),
+                                  onPressed: () {
+                                    //After successful login we will redirect to profile page. Let's create profile page now
+                                    verifyUser();
+                                    // Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => ProfilePage() ));
+                                  },
+                                ),
+                              ),
+                              Container(
+                                margin:
+                                    const EdgeInsets.fromLTRB(10, 20, 10, 20),
+                                //child: Text('Don\'t have an account? Create'),
+                                child: Text.rich(TextSpan(children: [
+                                  const TextSpan(
+                                      text: "Don\'t have an account? "),
+                                  TextSpan(
+                                    text: 'Create',
+                                    recognizer: TapGestureRecognizer()
+                                      ..onTap = () {
+                                        Navigator.push(
+                                            context,
+                                            MaterialPageRoute(
+                                                builder: (context) =>
+                                                    RegistrationPage()));
+                                      },
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        color: Theme.of(context).accentColor),
+                                  ),
+                                ])),
+                              ),
+                            ],
+                          )),
+                    ],
+                  )),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future verifyUser() async {
+    final uri = Uri.parse('http://192.168.254.143:80/api/verifyUser.php');
+    final headers = {'Content-Type': 'application/json'};
+    Map<String, dynamic> body = {'email': user.text, 'password': pass.text};
+    // String jsonBody = json.encode(body);
+
+    var response = await http.post(
+      uri,
+      headers: headers,
+      body: json.encode(body),
+    );
+
+    int statusCode = response.statusCode;
+    String responseBody = response.body;
+    if (responseBody == "SUCCESS") {
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => ProfilePage()));
+    } else if (responseBody == "NOT_VERIFY") {
+      Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+              builder: (context) => ForgotPasswordVerificationPage()));
+    } else {
+      print('Response body: ${responseBody}');
+    }
+  }
+}
+
+class ObjectResult {}
